ee4c6103379e09e9b84b89bb651461df72c21593
ErikDaSilva19 <erikborgesdasilva999@gmail.com>
2025-04-03 17:04:29 -0300

    removido o botao de voltar da home

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -2,17 +2,9 @@ import React from 'react';
 import { Banner, Container, Content } from './styles.js';
 import { CategoriesCarousel, OffersCarousel } from '../../components';
 import { useUser } from '../../hooks/UserContext';
-import { useNavigate } from 'react-router-dom';
 export function Home() {
-  const navigate = useNavigate();
   return (
     <main>
-      <button
-        onClick={() => navigate('/admin/produtos')}
-        style={{ backgroundColor: 'transparent' }}
-      >
-        .
-      </button>
       <Banner>
         <h1>Bem-vindo(a)!!</h1>
       </Banner>
