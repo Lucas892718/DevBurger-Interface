fe193ab401dd3e60930122fcb62875bbcd56b115
ErikDaSilva19 <erikborgesdasilva999@gmail.com>
2025-04-04 14:49:27 -0300

    Correcao no toast no CartButton

--- src/components/CartButton/index.jsx
+++ src/components/CartButton/index.jsx
@@ -2,16 +2,20 @@ import React from 'react';
 import { ContainerButton } from './styles.js';
 import CartBtn from '../../assets/Cart.svg';
 import { toast } from 'react-toastify';
-export function CartButton({ ...props }) {
+import PropTypes from 'prop-types';
+
+export function CartButton({ onClick, ...props }) {
+  const handleClick = (e) => {
+    if (onClick) onClick(e); // chama a função que adiciona ao carrinho
+    toast.success('Produto adicionado ao carrinho');
+  };
   return (
-    <ContainerButton
-      {...props}
-      onClick={() => {
-        toast.success('Produto adicionado ao carrinho');
-        setTimeout(() => {}, 500);
-      }}
-    >
+    <ContainerButton onClick={handleClick} {...props}>
       <img src={CartBtn} alt="Carrinho-de-compras" />
     </ContainerButton>
   );
 }
+
+CartButton.propTypes = {
+  onClick: PropTypes.func
+};
