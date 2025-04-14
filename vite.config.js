aec31dc55ff0571e352efa784d598b0b35ea4c49
ErikDaSilva19 <erikborgesdasilva999@gmail.com>
2025-04-14 20:30:38 -0300

    Ajuste do vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import babelPluginStyledComponents from 'babel-plugin-styled-components';
 
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: ['babel-plugin-styled-components'] // ✅ Adicionado aqui
+        plugins: [babelPluginStyledComponents]
       }
     })
   ]
